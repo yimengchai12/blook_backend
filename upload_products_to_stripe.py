9be55e25c6915c0b99cc762f26a0daf9535c90c9
aldensmu <115695607+aldensmu@users.noreply.github.com>
2023-03-28 23:25:59 +0800

    added payment microservice, added a python script to upload all the products from our sql database also

---
+++ upload_products_to_stripe.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, Table, MetaData
+import stripe
+
+stripe.api_key = "sk_test_51MqXdHE3thje2p8MDiPdiAf9rL1wQHZirFYfmKIetPDBkvyX2avd9BtxfIJ1BpThFRSTyoBSGBbk48BQygVYXkWo00kAK2chaW"
+# create a connection to the database
+engine = create_engine('mysql+mysqlconnector://root:root@localhost:3306/activity')
+
+# create a metadata object and reflect the database schema
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+# get a reference to the table you want to iterate over
+my_table = Table('activity', metadata, autoload=True, autoload_with=engine)
+
+# create a select statement to retrieve all rows from the table
+stmt = my_table.select()
+
+# execute the select statement and retrieve the result set
+with engine.connect() as conn:
+    result_set = conn.execute(stmt)
+
+    # iterate over the rows in the result set
+    for row in result_set:
+        # process each row as needed
+        stripe.Product.create(
+            id=row[0],
+            name=row[1],
+            default_price_data={
+                "unit_amount": int(row[3]*100),
+                "currency": "sgd",
+            },
+             expand=["default_price"],
+        )
